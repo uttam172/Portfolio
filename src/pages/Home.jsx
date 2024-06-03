07e0b3163dddb8596c5da5e312e527c8c7ca8c0d
uttam172 <uttamdobariya17@gmail.com>
2024-06-03 11:46:21 +0530

    portfolio basic structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+
+{/*<div className="absolute top-28 left-0 right-0 z-10 items-center flex justify-center">
+    POPUP
+</div>*/}
+
+const Home = () => {
+    return (
+        <section className="w-full h-screen relative">
+
+            <canvas>
+                a
+            </canvas>
+
+        </section>
+    )
+}
+
+export default Home
