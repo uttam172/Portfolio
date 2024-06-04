4dba0293af7c1cb89ec6a415e80ff5014aab93a0
uttam172 <uttamdobariya17@gmail.com>
2024-06-04 09:18:33 +0530

    island added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
   server: {
     port: 3000, // Replace 3000 with your preferred port number
   },
